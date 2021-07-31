bb53f2dfd8c688d19cfac51acd770868437df013
root <tech@hamwe.org>
2021-07-31 13:19:30 +0300

    Transaction mif=gration fix

--- coop/migrations/0019_auto_20210715_1517.py
+++ coop/migrations/0019_auto_20210715_1517.py
@@ -15,25 +15,25 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Transaction',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('transaction_reference', models.CharField(max_length=160)),
-                ('transaction_category', models.CharField(max_length=120)),
-                ('amount', models.DecimalField(decimal_places=2, max_digits=12)),
-                ('entry_type', models.CharField(blank=True, max_length=64, null=True)),
-                ('created_by_name', models.CharField(blank=True, max_length=160, null=True)),
-                ('create_date', models.DateTimeField(auto_now_add=True)),
-                ('update_date', models.DateTimeField(auto_now=True)),
-                ('created_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='transaction_user', to=settings.AUTH_USER_MODEL)),
-                ('members', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='coop.CooperativeMember')),
-                ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'db_table': 'transaction',
-            },
-        ),
+        # migrations.CreateModel(
+        #     name='Transaction',
+        #     fields=[
+        #         ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+        #         ('transaction_reference', models.CharField(max_length=160)),
+        #         ('transaction_category', models.CharField(max_length=120)),
+        #         ('amount', models.DecimalField(decimal_places=2, max_digits=12)),
+        #         ('entry_type', models.CharField(blank=True, max_length=64, null=True)),
+        #         ('created_by_name', models.CharField(blank=True, max_length=160, null=True)),
+        #         ('create_date', models.DateTimeField(auto_now_add=True)),
+        #         ('update_date', models.DateTimeField(auto_now=True)),
+        #         ('created_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='transaction_user', to=settings.AUTH_USER_MODEL)),
+        #         ('members', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='coop.CooperativeMember')),
+        #         ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+        #     ],
+        #     options={
+        #         'db_table': 'transaction',
+        #     },
+        # ),
         migrations.RemoveField(
             model_name='loanrequest',
             name='member',
